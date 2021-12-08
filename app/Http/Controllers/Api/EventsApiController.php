deb5fd46912c72e56f18e1871bb6b1e08208e2b6
shubham.gupta <shubham.gupta@stacksports.com>
2021-12-08 12:05:01 +0530

    create event api updated

--- app/Http/Controllers/Api/EventsApiController.php
+++ app/Http/Controllers/Api/EventsApiController.php
@@ -45,10 +45,7 @@ class EventsApiController extends BaseController
                 ]);
             }
             DB::commit();
-            $imageFiles = File::where([
-                ['event_id' => $event->id],
-                ['type' => 'image']
-                ])->get();
+            $imageFiles = File::where('event_id', $event->id)->where('type', '=', 'image')->get();
             $event->images = $imageFiles;
             return $this->sendResponse($event, 'Event created successfully.');
         } catch (\Exception $e) {
