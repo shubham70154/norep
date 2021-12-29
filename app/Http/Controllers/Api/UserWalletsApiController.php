65980010a51879566c7c06f50cf3703f26cc471a
shubham gupta <gupta.shubham91@gmail.com>
2021-12-29 13:25:13 +0530

    user event assign

---
+++ app/Http/Controllers/Api/UserWalletsApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Event;
+use App\SubEvent;
+use App\User;
+use App\File;
+use App\UserJoinedEvent;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\BaseController as BaseController;
+use App\Helpers\Helper;
+use App\Http\Requests\Request as RequestsRequest;
+use DB, Validator, Illuminate\Support\Carbon;
+
+class UserWalletsApiController extends BaseController
+{
+    public function getUserWallet($user_id)
+    {
+        try {
+            if (isset($user_id) && !is_null($user_id))
+            {
+                $userEvents = Event::where([
+                    ['user_id', $user_id],
+                    ['status', 1]
+                ])->orderBy('start_date', 'DESC')->pluck('id')->toArray();
+
+                return $userEvents;
+            } else {
+                return $this->sendError('User not found.', ['error'=>'User id not found!']);
+            }
+            
+        } catch (\Exception $e) {
+            return $this->sendError('Oops something went wrong.', ['error'=> $e->getMessage()]);
+        }
+    }
+
+}
