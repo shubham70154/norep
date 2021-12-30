32b49d5318b5bb83bc4884756c7f75f93efffdd8
shubham gupta <gupta.shubham91@gmail.com>
2021-12-30 23:23:41 +0530

    user event assign

--- app/Http/Controllers/Api/UserWalletsApiController.php
+++ app/Http/Controllers/Api/UserWalletsApiController.php
@@ -162,22 +162,22 @@ class UserWalletsApiController extends BaseController
             $transactionResult = [];
             foreach($userTransaction as $transaction){
                 if(isset($transaction->deposite) && !is_null($transaction->deposite)) {
-                    $transactions['deposite'] = $transaction->deposite;
+                    $transaction->deposite = $transaction->deposite;
                 }
                 if(isset($transaction->withdraw) && !is_null($transaction->withdraw)) {
-                    $transactions['withdraw'] = $transaction->withdraw;
+                    $transaction->withdraw = $transaction->withdraw;
                 }
                 if(isset($transaction->joining_event_name) && !is_null($transaction->joining_event_name)) {
-                    $transactions['joining_event_name'] = $transaction->joining_event_name;
-                }
-                if(isset($transaction->transaction_date_time) && !is_null($transaction->transaction_date_time)) {
-                    $transactions['transaction_date_time'] = $transaction->transaction_date_time;
+                    $transaction->joining_event_name = $transaction->joining_event_name;
                 }
                 if(isset($transaction->amount_before_transaction) && !is_null($transaction->amount_before_transaction)) {
-                    $transactions['amount_before_transaction'] = $transaction->amount_before_transaction;
+                    $transaction->amount_before_transaction = $transaction->amount_before_transaction;
                 }
                 if(isset($transaction->amount_after_transaction) && !is_null($transaction->amount_after_transaction)) {
-                    $transactions['amount_after_transaction'] = $transaction->amount_after_transaction;
+                    $transaction->amount_after_transaction = $transaction->amount_after_transaction;
+                }
+                if(isset($transaction->transaction_date_time) && !is_null($transaction->transaction_date_time)) {
+                    $transaction->transaction_date_time = $transaction->transaction_date_time;
                 }
                 $transactionResult[] = $transactions;
             }
