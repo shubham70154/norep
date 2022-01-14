b037ef9911608f2d27789a0f38da249c3d4da6e0
shubham gupta <gupta.shubham91@gmail.com>
2022-01-14 18:24:11 +0530

    update api

--- app/Http/Controllers/Api/LeaderBoardsApiController.php
+++ app/Http/Controllers/Api/LeaderBoardsApiController.php
@@ -94,7 +94,7 @@ class LeaderBoardsApiController extends BaseController
                             $leaderboard['rank'] = 1;
                             $scoreboardArray[] = $leaderboard;
                         }
-                        $leaderboard['scoreboards'] = $scoreboardArray;
+                        $leaderboard->scoreboard = (object)$scoreboardArray;
                     }
                     
                     $allSubevents[] = $leaderboard;
