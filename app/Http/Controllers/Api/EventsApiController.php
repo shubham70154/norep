321c68e09b8ef8ceda44b2f8af0aaa0a46240a0d
shubham gupta <gupta.shubham91@gmail.com>
2022-01-19 14:59:32 +0530

    update api

--- app/Http/Controllers/Api/EventsApiController.php
+++ app/Http/Controllers/Api/EventsApiController.php
@@ -26,7 +26,7 @@ class EventsApiController extends BaseController
             $validator = Validator::make($request->all(), [
                 'name' => 'required',
                 'description' => 'required',
-                'price' => 'required|numeric|min:0',
+                'price' => 'required|numeric|min:4.5',
                 'start_date' => 'required|after_or_equal:today',
                 'start_time' => 'required',
                 'end_date' => 'after_or_equal:start_date',
