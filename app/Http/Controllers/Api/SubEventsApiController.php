d181f58aa865be53b08c8b1b6b820dbf512a140a
shubham gupta <gupta.shubham91@gmail.com>
2022-02-03 13:12:20 +0530

    validation message change

--- app/Http/Controllers/Api/SubEventsApiController.php
+++ app/Http/Controllers/Api/SubEventsApiController.php
@@ -371,10 +371,10 @@ class SubEventsApiController extends BaseController
                     $eventSpecify = EventSpecify::find($subevent->event_specified_id);
                 } else {
                     $subEventData = SubEvent::find($subevent->id);
-                   return $subEventSpecifylist = SubEventSpecify::where([
+                    $subEventSpecifylist = SubEventSpecify::where([
                                     ['sub_event_id', $subevent->id]
                                 ])->pluck('event_specified_id')->all();
-                    $eventSpecify = EventSpecify::whereIn('id', $subEventSpecifylist);
+                    $eventSpecify = EventSpecify::whereIn('id', $subEventSpecifylist)->get();
                 }
 
                 $imagefiles = DB::table('files')->where([
