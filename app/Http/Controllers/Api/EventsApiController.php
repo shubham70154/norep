5f2272cdba3723369c4fe08c48cfe52288cef31c
shubham gupta <gupta.shubham91@gmail.com>
2022-02-04 10:54:10 +0530

    event time check

--- app/Http/Controllers/Api/EventsApiController.php
+++ app/Http/Controllers/Api/EventsApiController.php
@@ -23,7 +23,7 @@ class EventsApiController extends BaseController
     public function create(Request $request)
     {
         try {
-            return Carbon::now();
+            //return Carbon::now();
             $validator = Validator::make($request->all(), [
                 'name' => 'required',
                 'description' => 'required',
