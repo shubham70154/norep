b24fc0eda16e6a99b0f6395f9fb90c2f470370eb
shubham gupta <gupta.shubham91@gmail.com>
2022-02-07 18:43:15 +0530

    email verify

--- app/Http/Controllers/Api/UserJoinedEventsApiController.php
+++ app/Http/Controllers/Api/UserJoinedEventsApiController.php
@@ -61,6 +61,13 @@ class UserJoinedEventsApiController extends BaseController
                 $this->saveNotification(null, $eventUserDetail->id, $title, $msg, $notificationResponse);
                 //Send Notification to event creator (end)
 
+                $email_data['subject'] = "NoRep: Joined New Event";
+                $email_data['email']  = $user->email;
+                $email_data['name']  = $user->name;
+                $email_data['page'] = "emails.user.join-event";
+                $email_data['url'] = $token;
+
+                $this->dispatch(new \App\Jobs\SendEmailJob($email_data));
                 return $this->sendResponse($result, 'Event joined successfully.');
             }
             //if user is joining virtual event (end)
@@ -82,50 +89,58 @@ class UserJoinedEventsApiController extends BaseController
                 
                 if (count($freeRefereeLists) > 0) {
                     DB::begintransaction();
-                    $request->request->add(['referee_id' => $freeRefereeLists[0]]);
-                    $result = UserJoinedEvent::create($request->all());
-
-                    // Update user transaction table (deposite start)
-                    $eventUserDetail = User::find($eventDetail->user_id);
-                    $eventtotalAmount = $eventUserDetail->total_amount + $request->amount;
-                    
-                    $depositeData = [
-                        'user_id' => $eventDetail->user_id,
-                        'joining_event_name' => $eventDetail->name,
-                        'amount_before_transaction' => $eventUserDetail->total_amount,
-                        'amount_after_transaction' => $eventtotalAmount,
-                        'deposite' => $request->amount,
-                        'transaction_type' => 'deposite'
-                    ];
-                    $eventUserDetail->total_amount = $eventtotalAmount;
-                    $eventUserDetail->save();
-                    $userTransaction = UserTransaction::create($depositeData);
-                    // Update user transaction table (deposite end)
-
-                    DB::commit();
-                    //Send Notification to event creator (start)
                     $joinedUserDetail = User::find($request->user_id);
-                    $title = "Norep : A new Athlete has joined the event";
-                    $msg = "A new Athlete (". ucfirst($joinedUserDetail->name).") has joined the event ". "$eventDetail->name" ."." ;
-                    $notificationResponse = $this->sendNotification($eventUserDetail->device_token, $title, $msg);
-                    $this->saveNotification(null, $eventUserDetail->id, $title, $msg, $notificationResponse);
-                    //Send Notification to event creator (end)
+                    // $request->request->add(['referee_id' => $freeRefereeLists[0]]);
+                    // $result = UserJoinedEvent::create($request->all());
 
-                    //Send Notification to Judge/Referee (start)
-                    $judgeDetail = User::find($freeRefereeLists[0]);
-                    $title = "Norep: You have been invited to judge the new event";
-                    $msg = "You have been invited to judge the new event ". "$eventDetail->name" ."." ;
-                    $notificationResponse = $this->sendNotification($judgeDetail->device_token, $title, $msg);
-                    $this->saveNotification($judgeDetail->id, null, $title, $msg, $notificationResponse);
-                    //Send Notification to Judge/Referee (end)
+                    // // Update user transaction table (deposite start)
+                    // $eventUserDetail = User::find($eventDetail->user_id);
+                    // $eventtotalAmount = $eventUserDetail->total_amount + $request->amount;
+                    
+                    // $depositeData = [
+                    //     'user_id' => $eventDetail->user_id,
+                    //     'joining_event_name' => $eventDetail->name,
+                    //     'amount_before_transaction' => $eventUserDetail->total_amount,
+                    //     'amount_after_transaction' => $eventtotalAmount,
+                    //     'deposite' => $request->amount,
+                    //     'transaction_type' => 'deposite'
+                    // ];
+                    // $eventUserDetail->total_amount = $eventtotalAmount;
+                    // $eventUserDetail->save();
+                    // $userTransaction = UserTransaction::create($depositeData);
+                    // // Update user transaction table (deposite end)
 
-                    //Send Notification to event creator, The invited judge has accepted the invitation to become a referee (start)
-                    $judgeDetail = User::find($freeRefereeLists[0]);
-                    $title = "Norep: The invited judge has accepted the invitation to become a referee";
-                    $msg = "The invited judge (". ucfirst($judgeDetail->name).") has accepted the invitation to become a referee for event ". "$eventDetail->name" ."." ;
-                    $notificationResponse = $this->sendNotification($eventUserDetail->device_token, $title, $msg);
-                    $this->saveNotification(null, $eventUserDetail->id, $title, $msg, $notificationResponse);
-                    //Send Notification to event creator, The invited judge has accepted the invitation to become a referee (end)
+                    // DB::commit();
+                    // //Send Notification to event creator (start)
+                    // $joinedUserDetail = User::find($request->user_id);
+                    // $title = "Norep : A new Athlete has joined the event";
+                    // $msg = "A new Athlete (". ucfirst($joinedUserDetail->name).") has joined the event ". "$eventDetail->name" ."." ;
+                    // $notificationResponse = $this->sendNotification($eventUserDetail->device_token, $title, $msg);
+                    // $this->saveNotification(null, $eventUserDetail->id, $title, $msg, $notificationResponse);
+                    // //Send Notification to event creator (end)
+
+                    // //Send Notification to Judge/Referee (start)
+                    // $judgeDetail = User::find($freeRefereeLists[0]);
+                    // $title = "Norep: You have been invited to judge the new event";
+                    // $msg = "You have been invited to judge the new event ". "$eventDetail->name" ."." ;
+                    // $notificationResponse = $this->sendNotification($judgeDetail->device_token, $title, $msg);
+                    // $this->saveNotification($judgeDetail->id, null, $title, $msg, $notificationResponse);
+                    // //Send Notification to Judge/Referee (end)
+
+                    // //Send Notification to event creator, The invited judge has accepted the invitation to become a referee (start)
+                    // $judgeDetail = User::find($freeRefereeLists[0]);
+                    // $title = "Norep: The invited judge has accepted the invitation to become a referee";
+                    // $msg = "The invited judge (". ucfirst($judgeDetail->name).") has accepted the invitation to become a referee for event ". "$eventDetail->name" ."." ;
+                    // $notificationResponse = $this->sendNotification($eventUserDetail->device_token, $title, $msg);
+                    // $this->saveNotification(null, $eventUserDetail->id, $title, $msg, $notificationResponse);
+                    // //Send Notification to event creator, The invited judge has accepted the invitation to become a referee (end)
+
+                    $email_data['subject'] = "NoRep: Joined New Event";
+                    $email_data['user_details']  = $joinedUserDetail;
+                    $email_data['event_details']  = $eventDetail;
+                    $email_data['page'] = "emails.user.join-event";
+                
+                    $this->dispatch(new \App\Jobs\SendEmailJob($email_data));
                     return $this->sendResponse($result, 'Event joined successfully.');
                 } else {
                     return $this->sendResponse((object)[], "All referee's assigned, can't join event.");
