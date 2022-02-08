a6700c1e105f0b6025ded0d1bf6ecfe3f2ed6b08
shubham gupta <gupta.shubham91@gmail.com>
2022-02-08 20:54:53 +0530

    user_leaderboard_id added

--- routes/api.php
+++ routes/api.php
@@ -13,98 +13,101 @@ Route::get('email/verify/{id}/{hash}', 'Api\VerificationController@verify')->nam
 
 
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::apiResource('permissions', 'Api\PermissionsApiController');
+    //only verified account can access with this group
+    Route::group(['middleware' => ['verified']], function() {
+        Route::apiResource('permissions', 'Api\PermissionsApiController');
 
-    Route::post('save-device-token', 'Api\RegisterController@saveDeviceToken');
-    
-    Route::apiResource('roles', 'Api\RolesApiController');
+        Route::post('save-device-token', 'Api\RegisterController@saveDeviceToken');
+        
+        Route::apiResource('roles', 'Api\RolesApiController');
 
-    Route::apiResource('users', 'Api\UsersApiController');
+        Route::apiResource('users', 'Api\UsersApiController');
 
-    // user profile update
-    Route::post('user-profile-update/{userid}', 'Api\RegisterController@userProfileUpdate');
+        // user profile update
+        Route::post('user-profile-update/{userid}', 'Api\RegisterController@userProfileUpdate');
 
-    // Events API routes
-    Route::post('create-events', 'Api\EventsApiController@create');
-    Route::delete('delete-event', 'Api\EventsApiController@deleteEvent');
-    Route::delete('delete-sub-event', 'Api\SubEventsApiController@deleteSubEvent');
-    Route::get('get-allevent-list', 'Api\EventsApiController@getAllEventList');
-    Route::get('get-allactiveevent-list', 'Api\EventsApiController@getAllActiveEventList');
-    Route::get('get-pastevent-list/{userid?}', 'Api\EventsApiController@getPastEventList');
-    Route::get('get-futureevent-list/{userid?}', 'Api\EventsApiController@getFutureEventList');
-    Route::get('get-runningevent-list/{userid?}', 'Api\EventsApiController@getRunningEventList');
-    Route::get('get-unpublished-event-list-for-event-creator/{userid}', 'Api\EventsApiController@getUnpublishedEventListForEventCreator');
-    Route::get('event/{id}', 'Api\EventsApiController@showEventDetails');
+        // Events API routes
+        Route::post('create-events', 'Api\EventsApiController@create');
+        Route::delete('delete-event', 'Api\EventsApiController@deleteEvent');
+        Route::delete('delete-sub-event', 'Api\SubEventsApiController@deleteSubEvent');
+        Route::get('get-allevent-list', 'Api\EventsApiController@getAllEventList');
+        Route::get('get-allactiveevent-list', 'Api\EventsApiController@getAllActiveEventList');
+        Route::get('get-pastevent-list/{userid?}', 'Api\EventsApiController@getPastEventList');
+        Route::get('get-futureevent-list/{userid?}', 'Api\EventsApiController@getFutureEventList');
+        Route::get('get-runningevent-list/{userid?}', 'Api\EventsApiController@getRunningEventList');
+        Route::get('get-unpublished-event-list-for-event-creator/{userid}', 'Api\EventsApiController@getUnpublishedEventListForEventCreator');
+        Route::get('event/{id}', 'Api\EventsApiController@showEventDetails');
 
-    Route::put('event-update/{id}', 'Api\EventsApiController@eventUpdate');
+        Route::put('event-update/{id}', 'Api\EventsApiController@eventUpdate');
 
-    Route::put('sub-event-update/{id}', 'Api\SubEventsApiController@subEventUpdate');
+        Route::put('sub-event-update/{id}', 'Api\SubEventsApiController@subEventUpdate');
 
-    Route::get('get-event-creators-event-list/{user_id}', 'Api\EventsApiController@getEventCreatorsEventList');
+        Route::get('get-event-creators-event-list/{user_id}', 'Api\EventsApiController@getEventCreatorsEventList');
 
-    Route::post('create-subevents', 'Api\SubEventsApiController@createSubEvent');
-    Route::get('subevent/{subeventid}', 'Api\SubEventsApiController@showSubEventDetails');
-    Route::get('get-subevent-list/{eventid}/{event_specified_id?}', 'Api\SubEventsApiController@getSubEventList');
+        Route::post('create-subevents', 'Api\SubEventsApiController@createSubEvent');
+        Route::get('subevent/{subeventid}', 'Api\SubEventsApiController@showSubEventDetails');
+        Route::get('get-subevent-list/{eventid}/{event_specified_id?}', 'Api\SubEventsApiController@getSubEventList');
 
-    Route::post('save-help-support', 'Api\PagesApiController@saveHelpSupport');
-    Route::get('get-support-list', 'Api\PagesApiController@getSupportList');
+        Route::post('save-help-support', 'Api\PagesApiController@saveHelpSupport');
+        Route::get('get-support-list', 'Api\PagesApiController@getSupportList');
 
-    Route::post('save-terms-conditions', 'Api\RegisterController@saveTermCondition');
+        Route::post('save-terms-conditions', 'Api\RegisterController@saveTermCondition');
 
-    Route::get('get-referees-list', 'Api\RegisterController@getRefereesList');
+        Route::get('get-referees-list', 'Api\RegisterController@getRefereesList');
 
-    Route::get('referee-allocate-events/{refereeid}', 'Api\EventsApiController@refereeAllocatedEvents');
-    
-    Route::post('assign-event-referees', 'Api\EventsApiController@assignEventReferees');
+        Route::get('referee-allocate-events/{refereeid}', 'Api\EventsApiController@refereeAllocatedEvents');
+        
+        Route::post('assign-event-referees', 'Api\EventsApiController@assignEventReferees');
 
-    Route::post('update-event-player-limit', 'Api\EventsApiController@updateEventPlayerLimit');
+        Route::post('update-event-player-limit', 'Api\EventsApiController@updateEventPlayerLimit');
 
-    Route::post('join-user-event', 'Api\UserJoinedEventsApiController@joinUserEvent');
-    
-    Route::post('save-event-amount', 'Api\EventPaymentsApiController@saveEventAmount');
+        Route::post('join-user-event', 'Api\UserJoinedEventsApiController@joinUserEvent');
+        
+        Route::post('save-event-amount', 'Api\EventPaymentsApiController@saveEventAmount');
 
-    Route::get('get-participants-list-by-eventId/{eventid}', 'Api\UserJoinedEventsApiController@getParticipantsListByEventId');
+        Route::get('get-participants-list-by-eventId/{eventid}', 'Api\UserJoinedEventsApiController@getParticipantsListByEventId');
 
-    Route::get('get-referee-list-by-eventId/{eventid}', 'Api\UserJoinedEventsApiController@getRefereeListByEventId');
+        Route::get('get-referee-list-by-eventId/{eventid}', 'Api\UserJoinedEventsApiController@getRefereeListByEventId');
 
-    Route::get('get-joined-events-list-by-userId/{userid}', 'Api\UserJoinedEventsApiController@getJoinedEventsListByUserId');
-    
-    Route::post('check-user-joined-events', 'Api\UserJoinedEventsApiController@checkUserJoinedEvents');
+        Route::get('get-joined-events-list-by-userId/{userid}', 'Api\UserJoinedEventsApiController@getJoinedEventsListByUserId');
+        
+        Route::post('check-user-joined-events', 'Api\UserJoinedEventsApiController@checkUserJoinedEvents');
 
-    Route::get('get-event-timelines/{event_id}', 'Api\EventsApiController@getEventTimelines');
+        Route::get('get-event-timelines/{event_id}', 'Api\EventsApiController@getEventTimelines');
 
-    Route::get('get-subevent-category-lists', 'Api\EventsApiController@getSubEventCategoryLists');
+        Route::get('get-subevent-category-lists', 'Api\EventsApiController@getSubEventCategoryLists');
 
-    Route::get('get-event-specified-lists/{event_id}', 'Api\EventsApiController@getEventSpecifiedLists');
+        Route::get('get-event-specified-lists/{event_id}', 'Api\EventsApiController@getEventSpecifiedLists');
 
-    Route::get('get-event-leaderboard/{event_id}', 'Api\LeaderBoardsApiController@getEventLeaderBoard');
+        Route::get('get-event-leaderboard/{event_id}', 'Api\LeaderBoardsApiController@getEventLeaderBoard');
 
-    Route::get('get-user-wallet/{user_id}', 'Api\UserWalletsApiController@getUserWallet');
+        Route::get('get-user-wallet/{user_id}', 'Api\UserWalletsApiController@getUserWallet');
 
-    Route::get('event-joined-participant-lists/{event_id}', 'Api\UserWalletsApiController@eventJoinedParticipantLists');
+        Route::get('event-joined-participant-lists/{event_id}', 'Api\UserWalletsApiController@eventJoinedParticipantLists');
 
-    Route::post('user-wallet-deposite-amount', 'Api\UserWalletsApiController@userWalletDepositeAmount');
+        Route::post('user-wallet-deposite-amount', 'Api\UserWalletsApiController@userWalletDepositeAmount');
 
-    Route::post('user-wallet-withdraw-amount', 'Api\UserWalletsApiController@userWalletWithDrawAmount');
+        Route::post('user-wallet-withdraw-amount', 'Api\UserWalletsApiController@userWalletWithDrawAmount');
 
-    Route::post('referee-get-subevent-details', 'Api\RefereesApiController@refereeGetSubeventDetails');
+        Route::post('referee-get-subevent-details', 'Api\RefereesApiController@refereeGetSubeventDetails');
 
-    Route::post('add-user-score-by-referee/{user_leaderboardid?}', 'Api\RefereesApiController@addUserScoreByReferee');
+        Route::post('add-user-score-by-referee/{user_leaderboardid?}', 'Api\RefereesApiController@addUserScoreByReferee');
 
-    Route::post('submit-final-athlete-score-by-event-creator', 'Api\RefereesApiController@submitFinalAthleteScoreByEventCreator');
+        Route::post('submit-final-athlete-score-by-event-creator', 'Api\RefereesApiController@submitFinalAthleteScoreByEventCreator');
 
-    Route::post('submit-final-user-score-by-referee', 'Api\RefereesApiController@submitFinalUserScoreByReferee');
+        Route::post('submit-final-user-score-by-referee', 'Api\RefereesApiController@submitFinalUserScoreByReferee');
 
-    Route::post('user-transaction-details', 'Api\UserWalletsApiController@userTransactionDetails');
+        Route::post('user-transaction-details', 'Api\UserWalletsApiController@userTransactionDetails');
 
-    Route::post('user-event-leaderboard', 'Api\LeaderBoardsApiController@userEventLeaderboard');
+        Route::post('user-event-leaderboard', 'Api\LeaderBoardsApiController@userEventLeaderboard');
 
-    Route::get('get-referee-notification-list/{referee_id}', 'Api\NotificationsApiController@getRefereeNotificationList');
+        Route::get('get-referee-notification-list/{referee_id}', 'Api\NotificationsApiController@getRefereeNotificationList');
 
-    Route::delete('delete-referee-notification-list', 'Api\NotificationsApiController@deleteRefereeNotificationList');
+        Route::delete('delete-referee-notification-list', 'Api\NotificationsApiController@deleteRefereeNotificationList');
 
-    Route::get('get-event-creator-notification-list/{user_id}', 'Api\NotificationsApiController@getEventCreatorNotificationList');
+        Route::get('get-event-creator-notification-list/{user_id}', 'Api\NotificationsApiController@getEventCreatorNotificationList');
 
-    Route::delete('delete-event-creator-notification-list', 'Api\NotificationsApiController@deleteEventCreatorNotificationList');
+        Route::delete('delete-event-creator-notification-list', 'Api\NotificationsApiController@deleteEventCreatorNotificationList');
+    });
 });
 
