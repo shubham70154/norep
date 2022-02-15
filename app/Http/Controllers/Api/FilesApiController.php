57d751de485af635cb69e368ef029cc5692dbd16
shubham gupta <gupta.shubham91@gmail.com>
2022-02-15 20:16:01 +0530

    file upload issue fix

--- app/Http/Controllers/Api/FilesApiController.php
+++ app/Http/Controllers/Api/FilesApiController.php
@@ -24,11 +24,7 @@ class FilesApiController extends BaseController
                 $fileName = time() . '_' . $file->getClientOriginalName();
                 $filePath = 'norepFiles/'.$fileName;
                 // Save File on S3 Bucket.
-                $stream = Storage::disk('s3')->getDriver()
-                             ->readStream($file);
-
-Storage::disk('sftp')->put($filePath, $stream);
-               // Storage::disk('s3')->put($filePath, fopen($file, 'r+'));
+                Storage::disk('s3')->put($filePath, fopen($file, 'r+'));
                 $url = asset(Storage::disk('s3')->url($filePath));
     
                 if ($url) {
