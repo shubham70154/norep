248ad2d07228a1a50d8fb8f1bcff71842e2598c1
shubham gupta <gupta.shubham91@gmail.com>
2022-02-28 15:30:16 +0530

    paypal payout implemented

--- app/Http/Controllers/Api/UserJoinedEventsApiController.php
+++ app/Http/Controllers/Api/UserJoinedEventsApiController.php
@@ -38,6 +38,7 @@ class UserJoinedEventsApiController extends BaseController
             //if user is joining virtual event (start)
             if ($eventDetail->event_type_id == 1) {
                 DB::begintransaction();
+                $request->paypal_response =  json_encode($request->paypal_response);
                 $result = UserJoinedEvent::create($request->all());
                 $eventUserDetail = User::find($eventDetail->user_id);
 
@@ -84,6 +85,7 @@ class UserJoinedEventsApiController extends BaseController
                 if (count($freeRefereeLists) > 0) {
                     DB::begintransaction();
                     $request->request->add(['referee_id' => $freeRefereeLists[0]]);
+                    $request->paypal_response =  json_encode($request->paypal_response);
                     $result = UserJoinedEvent::create($request->all());
                     $eventUserDetail = User::find($eventDetail->user_id);
                     DB::commit();
