9355b932a72c3ccdc4472e5939b8084baf2c8472
shubham gupta <gupta.shubham91@gmail.com>
2022-04-15 11:45:03 +0530

    participant list updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,15 +13,32 @@
             </div>
             </div>
         </div>
-        <!-- <div class="col-sm-3 mb-3 mb-md-0">
+        <div class="col-sm-3 mb-3 mb-md-0">
             <div class="card text-center">
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                
-                <a href="#!" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Amount Earned : </h5>
+                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary">View Transaction</a>
             </div>
             </div>
-        </div> -->
+        </div>
+
+        <div class="col-sm-3 mb-3 mb-md-0">
+            <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">Events Created:  </h5>
+                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary">View All Events</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3 mb-3 mb-md-0">
+            <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">Total Users:  </h5>
+                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary">View All Users</a>
+            </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
