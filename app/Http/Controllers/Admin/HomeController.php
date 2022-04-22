5f7da21a6e8b118aa79300235136e8985f559ecd
shubham gupta <gupta.shubham91@gmail.com>
2022-04-22 14:11:51 +0530

    dashboard update

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\User;
+use App\UserTransaction;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController
 {
     public function index()
     {
-        $user = Auth::user(); 
+        $user = Auth::user();
+        $earnedAmount = UserTransaction::orderBy('id', 'DESC')->first(); 
 
-        return view('home', compact('user'));
+        return view('home', compact('user','earnedAmount'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
         <div class="col-sm-3 mb-3 mb-md-0">
             <div class="card text-center">
             <div class="card-body">
-                <h5 class="card-title">Amount Earned : </h5>
-                <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary">View Transaction</a>
+                <h5 class="card-title">Amount Earned : {{$earnedAmount->amount_after_transaction}} </h5>
+                <a href="{{ route('admin.walletmanagement.list', $user->id) }}" class="btn btn-primary">View Transaction</a>
             </div>
             </div>
         </div>
