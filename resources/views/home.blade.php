2426bfba984ac01f2be61f9a87ca6ddb77f72065
shubham gupta <gupta.shubham91@gmail.com>
2022-04-27 12:37:31 +0530

    admin transaction added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,8 @@
         <div class="col-sm-3 mb-3 mb-md-0">
             <div class="card text-center">
             <div class="card-body">
-                <h5 class="card-title">Amount Earned : {{$earnedAmount->amount_after_transaction}} </h5>
-                <a href="{{ route('admin.walletmanagement.list', 7) }}" class="btn btn-primary">View Transaction</a>
+                <h5 class="card-title">Admin Amount Earned : {{$earnedAmount->total_amount}} </h5>
+                <a href="{{ route('admin.walletmanagement.list', 1) }}" class="btn btn-primary">View Transaction</a>
             </div>
             </div>
         </div>
