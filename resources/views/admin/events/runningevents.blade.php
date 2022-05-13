174db23fa9d6f6552adbfe69ccef016b8ebed611
shubham gupta <gupta.shubham91@gmail.com>
2022-05-13 17:23:04 +0530

    design chnages push

--- resources/views/admin/events/runningevents.blade.php
+++ resources/views/admin/events/runningevents.blade.php
@@ -73,6 +73,11 @@
                                 <a class="btn btn-xs btn-info" href="{{ route('admin.events.subevent/lists', $event->id) }}">
                                         {{ trans('global.event.view_subevent') }}
                                 </a>
+
+                                <a class="btn btn-xs btn-primary" href="{{ route('admin.events.leaderboard', $event->id) }}">
+                                    View Leaderboard
+                                </a>
+                                
                                 <a class="btn btn-xs btn-info" href="{{ route('admin.events.participant/lists', $event->id) }}">
                                        Joined Participants
                                 </a>
